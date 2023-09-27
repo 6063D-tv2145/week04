0aa0c26824e0c84bc362bab935c287323e4f83e9
Thao Chi Vu <tv2145@nyu.edu>
2023-09-27 15:47:45 -0400

    class code

--- sketch.js
+++ sketch.js
@@ -1,6 +1,28 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background('gold')
+  fill(0);
+
 }
 
 function draw() {
+}
+
+function mouseClicked() {
+  fill(
+    random(0,600),random(0,600),random(0,600));
+    rect(mouseX, mouseY, 80, 80)
+}
+
+function keyPressed() {
+  if(key == 's') {
+  saveCanvas('myCanvas', '.jpg');
+  } else if (key == 'r'){
+  background('gold')
+}
+}
+
+function mouseDragged(){
+  stroke(0)
+  line(0,0, mouseX, mouseY)
 }
